669e7f394cb355b69c609a42508da278dd238753
Keti <ketikeet2@gmail.com>
2019-01-11 17:04:56 +0400

    changes in Logo

--- js/myCode.js
+++ js/myCode.js
@@ -31,3 +31,10 @@ $(document).ready(function(){
 
 
 });
+
+//change color of logo 
+$(document).ready(function(){
+  $('.toggleIcon').click(function(){
+    $('.AutoColor').toggleClass('redColor');
+  });
+});
